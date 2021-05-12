c952343e5a2f2026caf692d4a5b96eec2f0d6e41
GyuYoungCho <rbud613@gmail.com>
2021-05-12 23:53:31 +0900

    commit

--- src/com/baek/math/gol/B9080.java
+++ src/com/baek/math/gol/B9080.java
@@ -8,22 +8,33 @@ public class B9080 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		while(T-->0) {
 			int ans=0;
 			st = new StringTokenizer(br.readLine());
 			String time = st.nextToken();
 			int spd = Integer.parseInt(st.nextToken());
-			ans+=(spd/1440)*19000;
-			spd%=1440;
+			
 			st = new StringTokenizer(time,":");
-			int cur = Integer.parseInt(st.nextToken())*60+Integer.parseInt(st.nextToken());
+			int h = (Integer.parseInt(st.nextToken())+2)%24;
+			int m = Integer.parseInt(st.nextToken());
 		
 			while(spd>0) {
-				
+				if(h<=4&&spd>300) {
+					spd-=(600-(h*60+m));
+					h=10;
+					m=0;
+					ans+=5000;
+				}else {
+					h = (h+1)%24;
+					spd-=60;
+					ans+=1000;
+				}
 			}
-		
+			sb.append(ans).append("\n");
 		}
+		System.out.println(sb);
 	}
 
 }
